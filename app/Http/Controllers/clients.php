a7c9c526334c46d64a497755b91c884ec6f1832a
Medkit <105806939+Medk1t@users.noreply.github.com>
2023-11-27 12:22:11 +0500

    add models

--- app/Http/Controllers/clients.php
+++ app/Http/Controllers/clients.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\
 
 class clients extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = 
+        return $cli;
+    }
 }
