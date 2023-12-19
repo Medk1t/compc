aa7b237ccc2c860477a68a44e52f0b49adc5e20d
Medkit <105806939+Medk1t@users.noreply.github.com>
2023-12-19 13:50:28 +0500

    po

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class orderController extends Controller
 {
-    //
-}
+    public function index;
+    public store
+    public function update(Request $request)
+    {
+        $order = Event::find($request->id);
+        
+        $order->date;
+        $order->client_id;
+        $order->price;
+        $order->seat_id;
+        
+        $order->save();
+        return true;
+    }
+}   
+
