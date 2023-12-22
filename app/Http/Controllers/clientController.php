5527da7f1a47f4573133020c38cc467aef676dec
Medkit <105806939+Medk1t@users.noreply.github.com>
2023-12-22 16:11:51 +0500

    1

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -6,5 +6,64 @@ use Illuminate\Http\Request;
 
 class clientController extends Controller
 {
-    //
+    public function index()
+    {
+        $order = orderResources::collection(order::all());
+
+        return $order;
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function edit(Seller $seller)
+    {
+        //
+    }
+
+    public function update(Request $request, order $order)
+    {
+        if($request->id==null)
+        {
+            $order = new order;
+        
+            $order->date;
+            $order->client_id;
+            $order->price;
+            $order->seat_id;
+        
+            $order->save();
+            return true;
+        }
+        
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->id == null)
+        {
+            $order = new order;
+            $order->date;
+            $order->client;
+            $order->price;
+            $order->seat_id;
+            
+            $order->save();
+            return $order;
+        }
+    }
+
+    public function show($id)
+    {
+        $order = order::find($id);
+        return $sellers;
+    }
+
+    public function destroy($id)
+    {
+        order::destroy($id);
+        return true;
+    }
 }
--- app/Http/Controllers/seatController.php
+++ app/Http/Controllers/seatController.php
@@ -6,5 +6,64 @@ use Illuminate\Http\Request;
 
 class seatController extends Controller
 {
-    //
+    public function index()
+    {
+        $seat = seatResources::collection(seat::all());
+
+        return $seat;
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function edit(Seller $seller)
+    {
+        //
+    }
+
+    public function update(Request $request, seat $seat)
+    {
+        if($request->id==null)
+        {
+            $seat = new seat;
+        
+            $seat->date;
+            $seat->client_id;
+            $seat->price;
+            $seat->seat_id;
+        
+            $seat->save();
+            return true;
+        }
+        
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->id == null)
+        {
+            $seat = new seat;
+            $seat->date;
+            $seat->client;
+            $seat->price;
+            $seat->seat_id;
+            
+            $seat->save();
+            return $seat;
+        }
+    }
+
+    public function show($id)
+    {
+        $seat = seat::find($id);
+        return $sellers;
+    }
+
+    public function destroy($id)
+    {
+        seat::destroy($id);
+        return true;
+    }
 }
